7c7830e132b04f8349cee6fc5841ed148715b7b7
RODOLFO QUEIROZ <rodolfo.dqueiroz@gmail.com>
2021-03-21 10:05:43 -0300

    Instalando e Configurando o Entity Framework Core em um projeto .Net Core

---
+++ AngularTemplate.Data/Context/AngularTemplateContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AngularTemplate.Domain.Entities;
+using AngularTemplate.Data.Mappings;
+using AngularTemplate.Data.Extensions;
+
+namespace AngularTemplate.Data.Context
+{
+    public class AngularTemplateContext: DbContext
+    {
+        public AngularTemplateContext(DbContextOptions<AngularTemplateContext> options) : base(options)
+        {
+
+        }
+
+        #region "DbSets"
+
+        public DbSet<User> Users { get; set; }
+
+        #endregion
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UserMap());
+            modelBuilder.SeedData();
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
--- AngularTemplate/Startup.cs
+++ AngularTemplate/Startup.cs
@@ -2,9 +2,11 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using AngularTemplate.Data.Context;
 
 namespace AngularTemplate
 {
@@ -21,6 +23,10 @@ namespace AngularTemplate
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddEntityFrameworkNpgsql()
+                .AddDbContext<AngularTemplateContext>(options =>
+                options.UseNpgsql(Configuration.GetConnectionString("TemplateDb")).EnableSensitiveDataLogging());
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
